5b11a84607a8b2543ab0ce8e25e6b2b0f6678bd9
Ekamjot Singh <ejdhindsa@gmail.com>
2024-03-02 15:44:23 -0600

    Modifications Leading to Properly Running evaluate() in Hand and Driver

--- Fisbin.java
+++ Fisbin.java
@@ -1,5 +1,6 @@
 // import statements
 import java.util.Collections;
+import java.util.ArrayList;
 
 public class Fisbin
 {
@@ -13,6 +14,37 @@ public class Fisbin
         Collections.shuffle(deck.getCards());
         //System.out.println(deck);
         
+        // creating four players that will play the game, thus four instances of hand
+        Hand playerOne = new Hand(deckCreator(deck));
+        Hand playerTwo = new Hand(deckCreator(deck));
+        Hand playerThree = new Hand(deckCreator(deck));
+        Hand playerFour = new Hand(deckCreator(deck));
+        
+        // printing the players
+        System.out.println("Player 1:");
+        System.out.println(playerOne);
+        System.out.println("\nScore: " + playerOne.evaluate());
+        
+        
+        
+        // now creating a new arrayList of cards that will be added to the deck, this is done using a static method
+        
     } // end of main
+    
+    public static ArrayList<Card> deckCreator(Deck deck)
+    {
+        // creating an ArrayList for hand which
+        ArrayList<Card> hand = new ArrayList<>();
+        
+        // using a for loop to add cards to the deck
+        for (int i = 0; i < 10; i++)
+        {
+            hand.add(deck.deal());
+        } // end of for loop
+        
+        // returning the deck to main
+        return hand;
+        
+    }
     
 } // end of class
--- Hand.java
+++ Hand.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Hand extends SetOfCards
 {
@@ -14,7 +15,7 @@ public class Hand extends SetOfCards
         max = 10;
         
         // creating cards arraylist and checking for size
-        assert cardsAdded.size() > 10 : "Hand cannot have more than 10 cards.";
+        assert cardsAdded.size() < 11 : "Hand cannot have more than 10 cards.";
         
         this.cards = cardsAdded;
     } // end of full-arg constructor
@@ -31,15 +32,95 @@ public class Hand extends SetOfCards
         // copying the said arraylist and adding it to the new one
         ArrayList auxillaryArray = new ArrayList();
         
+        // creating value of sum
+        int sum = 0;
+        
         for(Card c : cards)
         {
             auxillaryArray.add(c);
         } // end of for-each
         
+        while(auxillaryArray.size() != 0)
+        {
+            sum += combinationChecker(auxillaryArray);
+        } // end of while loop
         
+        // nwo returning the sum as it is converted into an integer
+        return Integer.toString(sum);
         
-        
-        return "Check";
     } // end of evaluate()
+    
+    @Override
+    public String toString()
+    {
+        // creating a StringBuilder to achieve same output
+        StringBuilder st = new StringBuilder();
+        
+        for(int i = 0; i < 5; i++)
+        {
+            st.append(cards.get(i) + "\t");
+        } // end of for loop
+        
+        st.append("\n");
+        
+        for(int i = 5; i < 10; i++)
+        {
+            st.append(cards.get(i) + "\t");
+        } // end of for loop
+        
+        return st.toString();
+    }
+    
+    public static int combinationChecker(ArrayList<Card> cards)
+    {
+        // making a variable out of the first card in the deck to check it against other values
+        String faceValue = cards.get(0).getFace().getDisplay();
+        
+        // and since we have already selected a card, the count will be one
+        int count = 1;
+        
+        // creating an int that will returned as the sum of all the pairs
+        int sumPairs = 0;
+        
+        // first creating a count element to hold the amount of cards thata are in combinations
+        // this is done by looking through the array
+        for (int i = 1; i < cards.size(); i++)
+        {
+            if(faceValue.equals(cards.get(i).getFace().getDisplay()))
+            {
+                count++;                // increases the count by one
+                cards.remove(i);        // removes the card so that it is not counted again
+            } // end of if
+            
+        } // end of for loop
+        
+        // now if the value of the count is 2 or more, we perform the following operations
+        if (count == 2)
+        {
+            sumPairs = cards.get(0).getFace().getPoints() * 4;
+            sumPairs *= count;        // because there is that number of cards in the hand
+            cards.remove(0);         // card remove so no actions performed on it again
+            return sumPairs;
+        } // end of if
+        else if (count == 3)
+        {
+            sumPairs = cards.get(0).getFace().getPoints() * 7;
+            sumPairs *= count;        // because there is that number of cards in the hand
+            cards.remove(0);         // card remove so no actions performed on it again
+            return sumPairs;
+        } // end of else if
+        else if (count == 4)
+        {
+            sumPairs = cards.get(0).getFace().getPoints() * 10;
+            sumPairs *= count;        // because there is that number of cards in the hand
+            cards.remove(0);         // card remove so no actions performed on it again
+            return sumPairs;
+        } // end of else if
+        
+        // now working in the general case where the sum pair would be 0 since there are no matching cards
+        cards.remove(0);         // card remove so no actions performed on it again
+        return sumPairs;
+        
+    } // end of combinationChecker()
     
 } // end of class
